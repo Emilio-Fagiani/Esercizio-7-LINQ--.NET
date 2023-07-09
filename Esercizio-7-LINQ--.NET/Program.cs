13bce678b9c2a7da09d1ed56748095fb48c615ac
emifa <emifa@LAPTOP-1TM449QB>
2023-07-09 13:26:09 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-7-LINQ--.NET/Program.cs
@@ -0,0 +1,18 @@
+﻿//Se voglio solo 1 articolo e voglio assicurarmi che sia l'unico articolo con quel valore,
+//quale uso tra  FirstOrDefault e SingleOrDefault
+
+//FirstOrDefault
+//Questo metodo restituisce il primo elemento da una
+//raccolta che soddisfa una condizione specificata o
+//un valore predefinito se non viene trovato alcun elemento di questo tipo
+
+int[] num = { 1, 5, 4, 8, 9, 10 };
+int firstNum = num.FirstOrDefault( x => x % 2 == 0);
+Console.WriteLine(firstNum);
+
+//SingleOrDefault
+//Questo metodo restituisce l'unico elemento nell'array che soddisfa la condizione.
+//Se fossero presenti più elementi con il valore 3 o se nessun elemento corrispondesse alla condizione,
+// si generebbe una condizione
+int uniqueNum = num.SingleOrDefault( x => x == 1);
+Console.WriteLine(uniqueNum);   
